d1917b5f1822b6a439dcbad6b182eff87540271d
David Meister <thedavidmeister@gmail.com>
2017-05-10 23:10:45 +1000

    add some basic typo

--- src/pages/index.cljs
+++ src/pages/index.cljs
@@ -7,13 +7,25 @@
 
 (h/html
  (h/head
+  (h/link
+   :href "https://necolas.github.io/normalize.css/latest/normalize.css"
+   :rel "stylesheet"
+   :type "text/css")
   (fonts.google-fonts/fonts
    [{:name "Playfair Display"
      :fallback "serif"}
     {:name "Raleway"
-     :fallback "sans-serif"}]))
+     :fallback "sans-serif"}])
+  (h/link
+   :href "/app.css"
+   :rel "stylesheet"
+   :type "text/css"))
  (h/body
   :css {:background (str "linear-gradient(to left, " (clojure.string/join ", " (colours.ui-gradients/stops "Ash")) ")")
         :min-height "100vh"}
   (h/div
-   "Hello worldz")))
+   :css {:margin-left "10vw"
+         :margin-top "5vh"}
+   (h/h1 "Dim valley pty. ltd.")
+   (h/p
+    "Lorem ipsum."))))
