3378a445fd496c518086e2a57f53d3af0994a36d
David Meister <thedavidmeister@gmail.com>
2017-05-11 14:53:39 +1000

    wip on flower menu

--- src/menu/flower.cljs
+++ src/menu/flower.cljs
@@ -1,9 +1,24 @@
 (ns menu.flower
- (:require [hoplon.core :as h]))
+ (:require
+  [hoplon.core :as h]
+  [javelin.core :as j]))
 
 ; Based on https://codepen.io/jordanlachance/pen/yOJdRr
 
+(defn open-button
+ [open? width]
+ (let [open? (or open? (j/cell false))]
+  (h/div
+   :click #(swap! open? not)
+
+   "Menu")))
+
 (defn menu
  [items width]
- (h/for-tpl [item items]
-  (h/div item)))
+ (let [open? (j/cell false)]
+  (h/div
+   (open-button open? width)
+   (h/when-tpl open?
+    (h/div
+     (h/for-tpl [item items]
+      (h/div item)))))))
