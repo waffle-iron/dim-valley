ef76101287a48c54b72a3a5b5564457795946b44
David Meister <thedavidmeister@gmail.com>
2017-05-12 12:59:40 +1000

    messing around with content layouts

--- src/pages/milly.cljs
+++ src/pages/milly.cljs
@@ -1,64 +1,55 @@
 (ns pages.milly
  (:require
-  [hoplon.core :as h]))
+  [hoplon.core :as h]
+  layout.content-block))
 
 (defn content []
- (h/div
-  :css {
-        :padding (clojure.string/join " " [0 0 layout.config/spacer-height layout.config/spacer-width])
-        :background-color "white"
-        :border-width "4px"
-        :border-style "dotted"
-        :border-color (last (colours.ui-gradients/stops))
-        :border-image-source "url('border-dots.svg')"
-        :border-image-slice "33% 33%"
-        :border-image-repeat "round"
-        :background-clip "padding-box"
-        :border-right "none"}
+ (layout.content-block/content-outer
   (h/div
    {:css {:width "cacl(60vw - 4px)"}}
 
-   (h/div
-    :css (merge
-          {:margin "2.67rem 0"}
-          (fonts.hoplon/font-map->css-map fonts.config/playfair))
-    (h/h1 "Amelia Schmidt"
-     :css {:margin 0
-           :font-size "4rem"})
-    (h/span "I believe the modern business consciously develops and evolves its unique model and way of doing business such that, over time, the needs of the business and customer quite naturally become one and the same."
-     :css {:font-size "1rem"}))
+   (layout.content-block/content-inner
+    (h/div
+     :css (merge
+           {:margin "2.67rem 0"}
+           (fonts.hoplon/font-map->css-map fonts.config/playfair))
+     (h/h1 "Amelia Schmidt"
+      :css {:margin 0
+            :font-size "4rem"})
+     (h/span "I believe the modern business consciously develops and evolves its unique model and way of doing business such that, over time, the needs of the business and customer quite naturally become one and the same."
+      :css {:font-size "1rem"})))
 
    (h/img
     :src "https://pbs.twimg.com/media/C8DGQUCVYAABgc-.jpg:large"
-    :css {:width "calc(75vw - 4px)"
-          :margin-left "-5vw"})
+    :css {:width "100%"})
 
-   (h/h2 "User experience research")
+   (layout.content-block/content-inner
+    (h/h2 "User experience research")
 
-   (h/ul
-    "I can provide the following UX services:"
-    (h/li "Design Sprint, Lean and Design Thinking workshops and planning facilitator")
-    (h/li "User testing at all phases of the product lifecycle")
-    (h/li "User interface prototyping and wireframing")
-    (h/li "User experience research data synthesis"))
+    (h/ul
+     "I can provide the following UX services:"
+     (h/li "Design Sprint, Lean and Design Thinking workshops and planning facilitator")
+     (h/li "User testing at all phases of the product lifecycle")
+     (h/li "User interface prototyping and wireframing")
+     (h/li "User experience research data synthesis"))
 
-   (h/h2 "User interface engineering")
+    (h/h2 "User interface engineering")
 
-   (h/ul
-    "I can provide the following front-end engineering services:"
-    (h/li "Advanced HTML/CSS implementations of preprocessors, frameworks, and other abstractions")
-    (h/li "Custom JavaScript leveraging popular frameworks such as React, Angular, jQuery, etc.")
-    (h/li "Custom Clojure(Script) using the Hoplon UI framework (this site is built on Hoplon)"))
+    (h/ul
+     "I can provide the following front-end engineering services:"
+     (h/li "Advanced HTML/CSS implementations of preprocessors, frameworks, and other abstractions")
+     (h/li "Custom JavaScript leveraging popular frameworks such as React, Angular, jQuery, etc.")
+     (h/li "Custom Clojure(Script) using the Hoplon UI framework (this site is built on Hoplon)"))
 
-   (h/h2 "Modern business education")
+    (h/h2 "Modern business education")
 
-   (h/ul
-    "I can prepare and present educational courses on the following topics:"
-    (h/li "Agile project management")
-    (h/li "Design thinking")
-    (h/li "HTML, CSS and JavaScript")
-    (h/li "Web computer science fundamentals"))
+    (h/ul
+     "I can prepare and present educational courses on the following topics:"
+     (h/li "Agile project management")
+     (h/li "Design thinking")
+     (h/li "HTML, CSS and JavaScript")
+     (h/li "Web computer science fundamentals"))
 
-   (h/h2 "Public speaking, panel discussions, social change")
+    (h/h2 "Public speaking, panel discussions, social change")
 
-   (h/p "I am passionate about the next generation of coders being women."))))
+    (h/p "I am passionate about the next generation of coders being women.")))))
