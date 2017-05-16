7b71409b156601b9d42a4b59d035d2df4bbb9333
David Meister <thedavidmeister@gmail.com>
2017-05-16 15:27:16 +1000

    Deploy to github pages

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@ git fetch;
 boot gh-pages;
 git add .;
 git commit -am "Deploy to github pages";
+git subtree push --prefix gh-pages origin gh-pages;
 # https://stackoverflow.com/questions/13756055/git-subtree-subtree-up-to-date-but-cant-push/30715198#30715198
-git push origin `git subtree split --prefix gh-pages master`:gh-pages --force;
+# git push origin `git subtree split --prefix gh-pages master`:gh-pages --force;
