7484638ab2dfd82bfe3cd777774d62fe14061a71
David Meister <thedavidmeister@gmail.com>
2017-05-16 14:36:22 +1000

    wip on getting deploy.sh to work better with git

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-git pull;
+git fetch;
 boot gh-pages;
 git add .;
 git commit -am "Deploy to github pages";
+git subtree pull --prefix gh-pages origin gh-pages;
 git subtree push --prefix gh-pages origin gh-pages;
