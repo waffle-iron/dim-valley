20bf6db3bbd77373e4b541306f4264be7407964d
David Meister <thedavidmeister@gmail.com>
2017-05-21 21:22:11 +1000

    tweak deployment for circle

--- deploy.sh
+++ deploy.sh
@@ -13,9 +13,9 @@ git add .;
 git commit -am "Deploy to github pages";
 
 echo "deploying to remote gh-pages branch..."
-git subtree push --prefix gh-pages origin gh-pages;
+# git subtree push --prefix gh-pages origin gh-pages;
 # https://stackoverflow.com/questions/13756055/git-subtree-subtree-up-to-date-but-cant-push/30715198#30715198
-# git push origin `git subtree split --prefix gh-pages master`:gh-pages --force;
+git push origin `git subtree split --prefix gh-pages master`:gh-pages --force;
 
 # Keep origin/master up to date as well
 echo "pushing to master..."
