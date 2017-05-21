f7e3d8b4d664856182d0d4f9b579099d55e20017
Circle CI <thedavidmeister@gmail.com>
2017-05-21 11:42:59 +0000

    Deploy to github pages

--- index.html.out/pages/github.js
+++ index.html.out/pages/github.js
@@ -5,5 +5,5 @@ goog.require('cljs.core.constants');
 goog.require('hoplon.core');
 goog.require('layout.content_block');
 pages.github.content = (function pages$github$content(){
-return layout.content_block.content_inner.cljs$core$IFn$_invoke$arity$variadic(cljs.core.array_seq([(hoplon.core.div.cljs$core$IFn$_invoke$arity$1 ? hoplon.core.div.cljs$core$IFn$_invoke$arity$1("github") : hoplon.core.div.call(null,"github"))], 0));
+return layout.content_block.content_inner.cljs$core$IFn$_invoke$arity$variadic(cljs.core.array_seq([(hoplon.core.h1.cljs$core$IFn$_invoke$arity$1 ? hoplon.core.h1.cljs$core$IFn$_invoke$arity$1("github") : hoplon.core.h1.call(null,"github"))], 0));
 });
--- index.html.out/pages/legal.js
+++ index.html.out/pages/legal.js
@@ -5,5 +5,5 @@ goog.require('cljs.core.constants');
 goog.require('hoplon.core');
 goog.require('layout.content_block');
 pages.legal.content = (function pages$legal$content(){
-return layout.content_block.content_inner.cljs$core$IFn$_invoke$arity$variadic(cljs.core.array_seq([(hoplon.core.div.cljs$core$IFn$_invoke$arity$1 ? hoplon.core.div.cljs$core$IFn$_invoke$arity$1("legal") : hoplon.core.div.call(null,"legal"))], 0));
+return layout.content_block.content_inner.cljs$core$IFn$_invoke$arity$variadic(cljs.core.array_seq([(hoplon.core.h1.cljs$core$IFn$_invoke$arity$1 ? hoplon.core.h1.cljs$core$IFn$_invoke$arity$1("legal") : hoplon.core.h1.call(null,"legal"))], 0));
 });
